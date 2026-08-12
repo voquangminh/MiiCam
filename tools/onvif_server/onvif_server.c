1d4d6cc991821ff096e45eb111f3cb5c8a5414f4
voquangminh <voquangminh90@gmail.com>
2026-08-12 13:46:22 +0700

    Update onvif_server.c

--- tools/onvif_server/onvif_server.c
+++ tools/onvif_server/onvif_server.c
@@ -403,7 +403,7 @@ static int motor_move_relative(int dx, int dy)
     if (ptz.y + dy > Y_MAX)
         dy = Y_MAX - ptz.y;
     if (dx != 0) {
-        dir = dx > 0 ? 1 : 0;
+        dir = dx > 0 ? 0 : 1;
         dist = dx > 0 ? dx : -dx;
         rc = motor_ioctl_int(H_DIR_SET, &dir);
         if (rc < 0)
